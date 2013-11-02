a8bfbf4565e6e2b4f5a158095ff88df53564ce05
Tamar Rucham <tamarr@ripariandata.com>
2013-11-02 16:13:41 -0400

    results file to create an input json for a connected graph of the chars

--- data/results.py
+++ data/results.py
@@ -1,6 +1,37 @@
-print 'diff_hebrew_arabic = ', 0-(abs(6.40740740741 - 8.16666666667) + abs(5.33333333333 - 7.8))
-print 'diff_hebrew_latin = ', 0-(abs(6.40740740741 - 7.65384615385) + abs(5.33333333333 - 5.57692307692))
-print 'diff_hebrew_thai = ', 0-(abs(6.40740740741 - 8.85106382979) + abs(5.33333333333 - 17.7659574468))
-print 'diff_arabic_latin = ', 0-(abs(8.16666666667 - 7.65384615385) + abs(7.8 - 5.57692307692))
-print 'diff_arabic_thai = ', 0-(abs(8.16666666667 - 8.85106382979) + abs(7.8 - 17.7659574468))
-print 'diff_latin_thai = ', 0-(abs(7.65384615385 - 8.85106382979) + abs(5.57692307692 - 17.7659574468))
+import json
+
+def getDiff(char1, char2):
+	return 0-(abs(float(char1['lines']) - float(char2['lines'])) + abs(float(char1['curves']) - float(char2['curves'])))
+
+# def generateCharJson():
+inputFile = open('data/output.json')
+data = json.load(inputFile)
+inputFile.close()
+
+languages_info = data['languages']
+for language in languages_info:
+	names_arr = []
+	weights_arr = []
+
+	index = 0
+	chars_arr = language['chars']
+	for single_char in chars_arr:
+		index += 1
+		char_name = single_char['char']
+		names_arr.append(char_name)
+
+		# Now iterate through the rest of the chars and determine link weight
+		for i in range(index, len(chars_arr)):
+			current_char = chars_arr[i]
+			weight = getDiff(single_char, current_char)
+			result_dic = {"source" : char_name, "target" : current_char['char'], "value": weight}
+			weights_arr.append(result_dic)
+
+
+	outputFile = open('data/ '+language['language'] + '_char_relation.json', 'w')
+	outputFile.write('{\n"nodes":')
+	outputFile.write(json.dumps(names_arr,indent=4))
+	outputFile.write(',\n"links":')
+	outputFile.write(json.dumps(weights_arr,indent=4))
+	outputFile.write('}')
+
