f6ef74559c0a820ba722d01a94dc3f026868ef12
Tamar Rucham <tamarr@ripariandata.com>
2013-11-23 15:17:00 -0500

    reordering and shifting

--- visualization/js/heatmap.js
+++ visualization/js/heatmap.js
@@ -1,81 +1,87 @@
 function load(order) {
-   var width = 1050,
-       height = 650;
+ var width = 650,
+ height = 750;
 
    //height of each row in the heatmap
    var h = 50;
    //width of each column in the heatmap
-   var w = 80;
+   var w = 50;
 
    var order = [
-    "Greek", 
-    "Cyrillic", 
-    "Latin", 
-    "Hebrew", 
-    "Arabic", 
-    "Thai", 
-    "Telugu", 
-    "Tamil", 
-    "Malayalam", 
-    "Devanagari", 
-    "Gujarati"
-    ]
+   "Greek", 
+   "Cyrillic", 
+   "Latin", 
+   "Hebrew", 
+   "Arabic", 
+   "Thai", 
+   "Tamil", 
+   "Malayalam", 
+   "Telugu",
+   "Gujarati",
+   "Devanagari"
+   ]
 
    //attach a SVG element to the document's body
    var mySVG = d3.select("body")
-      .append("svg")
-      .attr("width", width) 
-      .attr("height", height)
-      .attr('class','heatmap');
+   .append("svg")
+   .attr("width", width) 
+   .attr("height", height)
+   .attr('class','heatmap');
 
    d3.json("data/languages_heatmap.json", function(error, data) {
-      var hash_lookup = [];
+    var hash_lookup = [];
 
       //define a color scale using the min and max expression values
       var colorScale = d3.scale.linear()
-        .domain([data.minData, data.maxData])
-        .range(["green", "white"]);
+      .domain([data.minData, data.maxData])
+      .range(["green", "white"]);
 
       //generate heatmap rows
       var heatmapRow = mySVG.selectAll(".heatmap")
-         .data(data.data)
-         .enter().append("g");
+      .data(data.data)
+      .enter().append("g");
 
       //generate heatmap columns
       var heatmapRects = heatmapRow
-         .selectAll(".rect")
-         .data(function(d) {
-            return d;
-         }).enter().append("svg:rect")
-         .attr('width',w)
-         .attr('height',h)
-         .attr('x', function(d) {
-            return (d[2] * w) + 100;
-         })
-         .attr('y', function(d) {
-            return (d[1] * h) + 50;
-         })
-         .style('fill',function(d) {
-            return colorScale(d[0]);
-         }).on("mouseover", function() {
-            d3.select(this).append("svg:title")
-            .text(function(d) {return order[d[1]] + " & " + order[d[2]];})
-            .attr("x", function(d) {return 0;})
-            .attr("y", function (d) {return 0;}); 
-         });
+      .selectAll(".rect")
+      .data(function(d) {
+        return d;
+      }).enter().append("svg:rect")
+      .attr('width',w)
+      .attr('height',h)
+      .attr('x', function(d) {
+        return (d[2] * w) + 90;
+      })
+      .attr('y', function(d) {
+        return (d[1] * h) + 100;
+      })
+      .style('fill',function(d) {
+        return colorScale(d[0]);
+      }).on("mouseover", function() {
+        d3.select(this).append("svg:title")
+        .text(function(d) {return order[d[1]] + " & " + order[d[2]];})
+        .attr("x", function(d) {return 0;})
+        .attr("y", function (d) {return 0;}); 
+      });
 
       var columnLabel = mySVG.selectAll(".colLabel")
-         .data(data.labels)
-         .enter().append('svg:text')
-         .attr('x', function(d,i) {
-            return ((i + 0.5) * w) + 100;
-         })
-         .attr('y', 30)
-         .attr('class','label')
-         .style('text-anchor','middle')
-         .text(function(d) {return d;});
+      .data(data.labels)
+      .enter().append('svg:text')
+      .attr('x', function(d,i) {
+        return getLabelX(i, w);
+      })
+      .attr('y', 100)
+      .attr('class','label')
+      .attr('transform',function(d, i) {
+        return 'rotate(-65 '+(getLabelX(i, w))+',90)';
+      })
+      .text(function(d) {return d;});
 
       return mySVG;
 
-   });
+    });
+}
+
+function getLabelX(i, w) {
+  return ((i + 0.5) * w) + 70;
 }
--- visualization/js/tree.js
+++ visualization/js/tree.js
@@ -5,7 +5,7 @@ var language_back_color = 'green';
 function generate_lang(data, svg){
     root_origin = 100;
     var height = 750;
-    var width = 450;
+    var width = 250;
     var color = language_color;
     var link_color = language_link_color;
 
@@ -16,11 +16,11 @@ function generate_lang(data, svg){
     "Hebrew", 
     "Arabic", 
     "Thai", 
-    "Telugu", 
     "Tamil", 
     "Malayalam", 
-    "Devanagari", 
-    "Gujarati"
+    "Telugu",
+    "Gujarati",
+    "Devanagari"
     ]
     
     root = data;
@@ -35,38 +35,38 @@ function generate_lang(data, svg){
         if(!d.children || d.children.length === 0)
             return null;
 
-    // Remove nodes with one child (single line) to avoid clutter
-    for (var i = 0; i < 5; i++) {
-        // Break if this is not a single line node OR if this is the origin 
-        // OR if this language has a script associated OR if this is a live script
-        if (!d.children || d.children.length !== 1) {
-            break;
+        // Remove nodes with one child (single line) to avoid clutter
+        for (var i = 0; i < 5; i++) {
+            // Break if this is not a single line node OR if this is the origin 
+            // OR if this language has a script associated OR if this is a live script
+            if (!d.children || d.children.length !== 1) {
+                break;
+            }
+            new_children = d.children[0].children; 
+            new_name = d.children[0].name;
+            d.children = new_children;
+            d.name = new_name;
         }
-        new_children = d.children[0].children; 
-        new_name = d.children[0].name;
-        d.children = new_children;
-        d.name = new_name;
-    }
 
-    return (!d.children || d.children.length === 0) ? null : d.children;
-});
+        return (!d.children || d.children.length === 0) ? null : d.children;
+    });
 
     var nodes = tree.nodes(data);
     // Set the root node to start from the tab
     nodes.forEach(function(d) {
-     var idx = order.indexOf(d.name)
+        var idx = order.indexOf(d.name)
 
-     if (d.name === data.name) {
-        d.y = 10; 
-        d.x = root_origin;
-    } else if (idx >= 0) {
-        d.y = 450;
-        d.x = 50 * idx + 120;
-    }
+        if (d.name === data.name) {
+            d.y = 10; 
+            d.x = root_origin;
+        } else if (idx >= 0) {
+            d.y = 250;
+            d.x = 50 * idx + 170;
+        }
 
-    if(d.name == "Greek") d.y -= 50;
-    else if (d.name == "Brāhmī" || d.name == "Nāgarī") d.x += 100;
-}); 
+        if(d.name == "Greek") d.y -= 50;
+        else if (d.name == "Brāhmī" || d.name == "Nāgarī") d.x += 100;
+    }); 
 
     var links = tree.links(nodes);
     var layoutRoot = svg.attr("width", width+100).attr("height", height + 100).attr('class','svg_position')
@@ -77,11 +77,8 @@ function generate_lang(data, svg){
 
     .projection(function(d)
     {
-     y = d.y;
-     x =  d.x;
-
-     return [y, x];
- });
+        return [d.y, d.x];
+    });
 
     layoutRoot.selectAll("path.link")
     .data(links)
@@ -89,18 +86,11 @@ function generate_lang(data, svg){
     .append("svg:path")
     .attr("class", "link")
     .attr('style', function(d){
-     return 'stroke: ' + link_color +'; ';
- })
+        return 'stroke: ' + link_color +'; ';
+    })
     .attr("d", link);
 
- /*
-     Nodes as
-     <g class="node">
-         <circle class="node-dot" />
-         <text />
-     </g>
-     */
-     var nodeGroup = layoutRoot.selectAll("g.node")
+    var nodeGroup = layoutRoot.selectAll("g.node")
      .data(nodes)
      .enter()
      .append("svg:g")
@@ -108,10 +98,7 @@ function generate_lang(data, svg){
      .attr("id", function(d) {return getID(d.name);})
      .attr("transform", function(d)
      {
-         y = d.y;
-         x = d.x;
-
-         return "translate(" + y + "," + x + ")";
+         return "translate(" + d.y + "," + d.x + ")";
      });
 
      nodeGroup.append("svg:circle")
@@ -132,11 +119,11 @@ function generate_lang(data, svg){
      {
         if (d.name !== data.name)
          return d.name;
-     })
+    })
      .attr('style', 'fill:'+color);
 
      return layoutRoot;
- }
+}
 
  function getID(name) {
     return name.replace(' ','') + '_node';
