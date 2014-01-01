0fba1a4ffa12612b79448628e73e111b0f534dab
Tamar Rucham <tamarr@ripariandata.com>
2013-12-31 22:10:42 -0500

    formatting the char relation page

--- visualization/js/char_relation.js
+++ visualization/js/char_relation.js
@@ -1,6 +1,6 @@
 var w = 500;
 var h = 400;
-var margin = 20;
+var margin = 50;
 var padding = 20;
 
 function getParameterByName(name) {
@@ -10,21 +10,74 @@ function getParameterByName(name) {
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+function distEclud(x1, y1, x2, y2){
+    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+}
+
+function findRepresentativeForCluster(clusters, chars) {
+    var rep_chars_arr = new Array();
+    // Go over each cluster
+    for (var cluster_idx = 0; cluster_idx < clusters.length; cluster_idx++) {
+        var cluster_pt = clusters[cluster_idx];
+        var rep_char_min = 100;
+        var rep_char = undefined;
+        // Find and store the minimal distance char
+        for (var i = 0; i < chars.length; i++) {
+            char_pt = chars[i];
+            dist = distEclud(char_pt[0],char_pt[1],cluster_pt[0],cluster_pt[1]);
+            if ( dist < rep_char_min) {
+                rep_char_min = dist;
+                rep_char = char_pt[3];
+            }
+        }
+
+        // Add the char closest to the cluster center to the representative chars array
+        rep_chars_arr[cluster_idx] = rep_char;
+    }
+    return rep_chars_arr;
+}
+
 function loadChars() {
     var script_name = getParameterByName("script");
     d3.json("data/chars_outputs/"+script_name+"_chars_output.json", function(error, dataset) {
         var chars = dataset['chars'];
+        var clusters_pts = dataset['clusters']
+        var rep_chars = findRepresentativeForCluster(clusters_pts, chars);
+
+        // Setup scales
         var fillColor = d3.scale.ordinal()
             .domain([0, 1, 2])
-            .range(["red", "blue", "green"]);
+            .range(["#CC0066", "#6666FF", "#669900"]);
 
         var xScale = d3.scale.linear()
-            .domain([0, 30])
+            .domain([0, 50])
             .range([padding, w]);
 
         var yScale = d3.scale.linear()
-            .domain([0, 30])
+            .domain([0, 50])
             .range([h - padding, padding]);
+
+        // Display representative chars
+        var rep_chars_div = d3.select("body")
+            .append("div")
+            .attr("id", "rep_chars_div")
+            .attr("width", w + margin)
+            .attr("height", 40);
+
+        rep_chars_div.selectAll("chars")
+            .data(rep_chars)
+            .enter()
+            .append("span")
+            .html(function(d){return d;})
+            .attr("class", "rep_char")
+            .attr("x", function(d){
+                return 40*rep_chars.indexOf(d);
+            })
+            .attr("y", "0")
+            .attr("style", function(d){
+                var color = fillColor(rep_chars.indexOf(d));
+                return "color:"+color+"; border: 2px solid "+color;
+            });
 
         //Create SVG element
         var svg = d3.select("body")
@@ -33,21 +86,7 @@ function loadChars() {
             .attr("width", w + margin)
             .attr("height", h + margin);
 
-        /*svg.selectAll("circle")
-           .data(chars)
-           .enter()
-           .append("circle")
-           .attr("cx", function(d) {
-                return xScale(d[0]);
-           })
-           .attr("cy", function(d) {
-                return yScale(d[1]);
-           })
-           .attr("r", 5)
-           .attr("style", function(d){
-                return "fill:"+fillColor(d[2]);
-           });*/
-
+        // Add char marks
         svg.selectAll("text")
            .data(chars)
            .enter()
@@ -61,12 +100,12 @@ function loadChars() {
            .attr("y", function(d) {
                 return yScale(d[1]);
            })
-           .attr("font-family", "sans-serif")
-           .attr("font-size", "1em")
+           .attr("class", "char_mark")
            .attr("style", function(d){
                 return "fill:"+fillColor(d[2]);
            });
 
+        // Add Axes
         var xAxis = d3.svg.axis()
             .scale(xScale)
             .orient("bottom")
@@ -87,9 +126,24 @@ function loadChars() {
             .attr("transform", "translate(" + padding + ",0)")
             .call(yAxis);
 
+        // Axes labels
+        svg.append("text")
+            .attr("class", "axis_label")
+            .attr("text-anchor", "end")
+            .attr("x", w + padding*2)
+            .attr("y", h - 10)
+            .text("Lines");
 
+        svg.append("text")
+            .attr("class", "axis_label")
+            .attr("text-anchor", "top")
+            .attr("x", 0)
+            .attr("y", 10)
+            .text("Curves");
+            
+        // Mark clusters centers
         svg.selectAll("clusters")
-           .data(dataset['clusters'])
+           .data(clusters_pts)
            .enter()
            .append("text")
            .attr("x", function(d) {
@@ -99,7 +153,9 @@ function loadChars() {
                 return yScale(d[1]);
            })
            .text("+")
-           .attr("font-size", "1.5em")
-           .attr("style", "fill:purple");
+           .attr("class", "cluster_mark")
+           .attr("style", function(d){
+                return "fill:"+fillColor(clusters_pts.indexOf(d));
+           });
     });
 }
